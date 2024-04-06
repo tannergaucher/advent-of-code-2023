f128cfbb246608508782878bade305e8a990a42a
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-04-06 10:37:07 -0400

    Day 2 completed

--- day-2/index.test.ts
+++ day-2/index.test.ts
@@ -1,13 +1,93 @@
 import { describe, it, expect } from "vitest";
 
-import { getInputRows } from "./index";
+import { getGamesFromInput, checkValidGame } from "./index";
 
 describe("get input rows", () => {
   it("returns an array of game hands from the input", () => {
-    const games = getInputRows("./input.txt");
-
-    console.log(JSON.stringify(games, null, 2));
+    const games = getGamesFromInput("./input.txt");
 
     expect(Array.isArray(games)).toBe(true);
+  });
+});
+
+describe("check valid game", () => {
+  it("returns true for a valid game (each hand has a valid color / number combination)", () => {
+    const valid = checkValidGame({
+      game: [
+        {
+          red: 7,
+          blue: 11,
+        },
+        {
+          red: 10,
+          blue: 5,
+          green: 1,
+        },
+        {
+          red: 7,
+          green: 1,
+          blue: 13,
+        },
+        {
+          red: 9,
+        },
+        {
+          red: 9,
+          blue: 17,
+        },
+        {
+          red: 9,
+          blue: 9,
+        },
+      ],
+      valid: {
+        green: 20,
+        blue: 18,
+        red: 40,
+      },
+    });
+
+    expect(valid).toBe(true);
+
+    console.log(valid);
+  });
+
+  it("returns false for an game with an invalid hand", () => {
+    const valid = checkValidGame({
+      game: [
+        {
+          red: 70,
+          blue: 11,
+        },
+        {
+          red: 10,
+          blue: 5,
+          green: 1,
+        },
+        {
+          red: 7,
+          green: 1,
+          blue: 13,
+        },
+        {
+          red: 9,
+        },
+        {
+          red: 9,
+          blue: 17,
+        },
+        {
+          red: 9,
+          blue: 9,
+        },
+      ],
+      valid: {
+        green: 20,
+        blue: 18,
+        red: 40,
+      },
+    });
+
+    expect(valid).toBe(false);
   });
 });
--- day-2/index.ts
+++ day-2/index.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 
-export function getInputRows(path: string) {
+type GameColor = "red" | "blue" | "green";
+type Game = { [K in GameColor]?: number };
+
+export function getGamesFromInput(path: string) {
   return fs
     .readFileSync(path)
     .toString()
@@ -10,32 +13,91 @@ export function getInputRows(path: string) {
 
       const games = game.split(";").map((game) => game.split(","));
 
-      type GameColor = "red" | "blue" | "green";
-      type GameArrElement = { [K in GameColor]?: number };
-
       return games.map((game) => {
-        const gameArr: GameArrElement[] = [];
-
-        let gameObj = {};
+        let gameObj: Game = {};
 
         game.map((hand) => {
           const number = parseInt(hand.trim().split(" ")[0]);
-          const color = hand.trim().split(" ")[1];
+          const color = hand.trim().split(" ")[1] as GameColor;
+
           gameObj[color] = number;
         });
 
-        gameArr.push(gameObj);
-
-        return gameArr;
+        return gameObj;
       });
     });
 }
 
-// checkValidGame:
-//  if every hand in the games array is valid, return that game id (array's index + 1)
-export function checkValidGame() {}
+export function checkValidGame({
+  game,
+  valid,
+}: {
+  game: Game[];
+  valid: {
+    red: number;
+    green: number;
+    blue: number;
+  };
+}) {
+  let gameValid: boolean = false;
+  let redValid: boolean = false;
+  let blueValid: boolean = false;
+  let greenValid: boolean = false;
 
-// iterate the game array
-// check if valid game
-// return the reduced total of the valid game ids
-export function getValidGameIdSum() {}
+  return game
+    .map((hand) => {
+      gameValid = false;
+      redValid = false;
+      blueValid = false;
+      greenValid = false;
+
+      const { red, green, blue } = hand;
+
+      if (red === undefined || red <= valid.red) {
+        redValid = true;
+      }
+
+      if (blue === undefined || blue <= valid.blue) {
+        blueValid = true;
+      }
+
+      if (green === undefined || green <= valid.green) {
+        greenValid = true;
+      }
+
+      if (redValid && blueValid && greenValid) {
+        gameValid = true;
+      }
+
+      return gameValid;
+    })
+    .every((game) => game === true);
+}
+
+export function getValidGameIdSum({ games }: { games: Game[][] }) {
+  const validGameIds: number[] = [];
+
+  games.forEach((game, index) => {
+    const valid = checkValidGame({
+      game,
+      valid: {
+        red: 12,
+        green: 13,
+        blue: 14,
+      },
+    });
+
+    if (valid) {
+      validGameIds.push(index + 1);
+    }
+  });
+
+  return validGameIds.reduce((acc, curr) => acc + curr);
+}
+
+// 2406
+console.log(
+  getValidGameIdSum({
+    games: getGamesFromInput("./input.txt"),
+  })
+);
