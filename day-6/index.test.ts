3bbb51a2675071eeb47a435da47e656c44f7e9be
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-04-11 09:58:09 -0400

    Update index.test.ts

--- day-6/index.test.ts
+++ day-6/index.test.ts
@@ -3,10 +3,10 @@ import { describe, it, expect } from "vitest";
 import { createRacesFromInput, getRacePossibilities } from "./index";
 
 describe("createRacesFromInput", () => {
-  it("should return an array of game objects", () => {
-    const games = createRacesFromInput("./input.txt");
+  it("should return an array of race objects", () => {
+    const races = createRacesFromInput("./input.txt");
 
-    expect(games).toEqual([
+    expect(races).toEqual([
       { time: 53, distance: 275 },
       { time: 71, distance: 1181 },
       { time: 78, distance: 1215 },
@@ -19,4 +19,6 @@ describe("getRacePossibilities", () => {
   it.only(
     "should return an array of objects which contain the time and the distance traveled for that time"
   );
+
+  const possibilities = getRacePossibilities();
 });
