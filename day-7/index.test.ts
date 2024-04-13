e0cf3e24ede1d29d5c465fbad83d13e248d9eab2
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2024-04-13 16:54:44 -0400

    WIP

--- day-7/index.test.ts
+++ day-7/index.test.ts
@@ -3,9 +3,10 @@ import { describe, expect, it } from "vitest";
 import {
   getHandsFromInput,
   getHandType,
-  getComparisonIndex,
   type Hand,
-  sortHandsByComparisonIndex,
+  sortHandsByRank,
+  groupByConsecutive,
+  sortRankedHands,
 } from "./index";
 
 describe("getHandsFromInput", () => {
@@ -22,7 +23,7 @@ describe("getHandType", () => {
       cards: "KKKKK",
     });
 
-    expect(handType).toBe("FiveOfAKind");
+    expect(handType).toBe("fiveOfAKind");
   });
 
   it("returns the correct hand type for 4 of a kind", () => {
@@ -30,7 +31,7 @@ describe("getHandType", () => {
       cards: "KK5KK",
     });
 
-    expect(handType).toBe("FourOfAKind");
+    expect(handType).toBe("fourOfAKind");
   });
 
   it("returns the correct hand type for 3 of a kind", () => {
@@ -38,7 +39,7 @@ describe("getHandType", () => {
       cards: "KK5K7",
     });
 
-    expect(handType).toBe("ThreeOfAKind");
+    expect(handType).toBe("threeOfAKind");
   });
 
   it("returns the correct hand type for a full house", () => {
@@ -46,7 +47,7 @@ describe("getHandType", () => {
       cards: "KK5K5",
     });
 
-    expect(handType).toBe("FullHouse");
+    expect(handType).toBe("fullHouse");
   });
 
   it("returns the correct hand type for two pair", () => {
@@ -54,7 +55,7 @@ describe("getHandType", () => {
       cards: "K75K5",
     });
 
-    expect(handType).toBe("TwoPair");
+    expect(handType).toBe("twoPair");
   });
 
   it("returns the correct hand type for one pair", () => {
@@ -62,7 +63,7 @@ describe("getHandType", () => {
       cards: "K75K4",
     });
 
-    expect(handType).toBe("OnePair");
+    expect(handType).toBe("onePair");
   });
 
   it("returns the correct hand type for high card", () => {
@@ -70,125 +71,150 @@ describe("getHandType", () => {
       cards: "K75Q4",
     });
 
-    expect(handType).toBe("HighCard");
+    expect(handType).toBe("highCard");
   });
 });
 
-describe("getComparisonIndex", () => {
-  it("gets the comparison index at the first card", () => {
-    const hands: Hand[] = [
-      ["12345", "100"],
-      ["34512", "200"],
-      ["K4712", "200"],
-      ["23451", "300"],
-    ];
+describe("sortHandsByRank", () => {
+  it("sorts hands by rank", () => {
+    const hands = getHandsFromInput("./input.txt");
+    const sortedByRank = sortHandsByRank({ hands });
 
-    const comparisonIndex = getComparisonIndex({ hands });
-
-    expect(comparisonIndex).toBe(0);
-  });
-
-  it("gets the comparison index at the second card", () => {
-    const hands: Hand[] = [
-      ["12345", "100"],
-      ["34512", "200"],
-      ["15782", "200"],
-      ["23451", "300"],
-    ];
-
-    const comparisonIndex = getComparisonIndex({ hands });
-
-    expect(comparisonIndex).toBe(1);
-  });
-
-  it("gets the comparison index at the third card", () => {
-    const hands: Hand[] = [
-      ["12345", "100"],
-      ["32519", "200"],
-      ["15782", "200"],
-      ["23451", "300"],
-    ];
-
-    const comparisonIndex = getComparisonIndex({ hands });
-
-    expect(comparisonIndex).toBe(2);
-  });
-
-  it("gets the comparison index at the fourth card", () => {
-    const hands: Hand[] = [
-      ["12745", "100"],
-      ["32519", "200"],
-      ["15782", "200"],
-      ["23451", "300"],
-    ];
-
-    const comparisonIndex = getComparisonIndex({ hands });
-
-    expect(comparisonIndex).toBe(3);
-  });
-
-  it("gets the comparison index at the fifth card", () => {
-    const hands: Hand[] = [
-      ["12745", "100"],
-      ["32589", "200"],
-      ["15782", "200"],
-      ["23451", "300"],
-    ];
-
-    const comparisonIndex = getComparisonIndex({ hands });
-
-    expect(comparisonIndex).toBe(4);
+    expect(Object.keys(sortedByRank).length).toBe(7);
   });
 });
 
-describe("sortHandsByComparisonIndex", () => {
-  it("sorts hands at comparison index 0", () => {
+describe("groupByConsecutive", () => {
+  it("groups hands by consecutive sequence", () => {
     const hands: Hand[] = [
-      ["12345", "100"],
-      ["34512", "200"],
-      ["K4712", "200"],
-      ["23451", "300"],
+      ["Q4Q7Q", "307"],
+      ["QQ6Q3", "252"],
+      ["QQQ79", "870"],
+      ["Q6JQQ", "742"],
+      ["QQQ28", "998"],
+      ["Q6AAA", "862"],
+      ["Q8886", "433"],
+      ["QQQ38", "411"],
+      ["QQQ76", "321"],
+      ["Q2QJQ", "396"],
+      ["QKQTQ", "54"],
     ];
 
-    const comparisonIndex = 0;
-
-    const expectedHands: Hand[] = [
-      ["K4712", "200"],
-      ["34512", "200"],
-      ["23451", "300"],
-      ["12345", "100"],
-    ];
-
-    const sortedHands = sortHandsByComparisonIndex({
+    const grouped = groupByConsecutive({
+      char: "Q",
       hands,
-      comparisonIndex,
     });
 
-    expect(sortedHands).toEqual(expectedHands);
+    // expect(grouped.length).toBe(2);
   });
+});
 
-  it("sorts hands at comparison index 1", () => {
-    const hands: Hand[] = [
-      ["12345", "100"],
-      ["34512", "200"],
-      ["38712", "200"],
-      ["23451", "300"],
+describe("sortRankedHands", () => {
+  it.only("sorts ranked hands", () => {
+    const unsortedRanked: Hand[] = [
+      ["25722", "304"],
+      ["3J777", "548"],
+      ["37776", "242"],
+      ["ATAA5", "750"],
+      ["99J9T", "916"],
+      ["Q4Q7Q", "307"],
+      ["TTAT8", "483"],
+      ["6T6J6", "823"],
+      ["229T2", "17"],
+      ["QQ6Q3", "252"],
+      ["QQQ79", "870"],
+      ["AKTKK", "297"],
+      ["Q6JQQ", "742"],
+      ["59699", "653"],
+      ["KK45K", "94"],
+      ["QQQ28", "998"],
+      ["373J3", "144"],
+      ["Q6AAA", "862"],
+      ["6669Q", "685"],
+      ["55985", "124"],
+      ["8TTT2", "337"],
+      ["T5T9T", "628"],
+      ["4J434", "53"],
+      ["T2722", "757"],
+      ["33732", "539"],
+      ["J8TTT", "933"],
+      ["AA9JA", "582"],
+      ["77JT7", "151"],
+      ["53K33", "290"],
+      ["Q8886", "433"],
+      ["K9599", "857"],
+      ["33J3T", "292"],
+      ["7T5TT", "639"],
+      ["98995", "593"],
+      ["88987", "108"],
+      ["3T33Q", "535"],
+      ["22TA2", "924"],
+      ["TTJTQ", "833"],
+      ["36662", "204"],
+      ["36566", "796"],
+      ["888TJ", "485"],
+      ["J9499", "610"],
+      ["39KKK", "606"],
+      ["97666", "509"],
+      ["J4KKK", "257"],
+      ["QQQ38", "411"],
+      ["AA3A2", "372"],
+      ["8QJQQ", "371"],
+      ["33Q43", "416"],
+      ["9A989", "407"],
+      ["A5AAJ", "7"],
+      ["J99A9", "305"],
+      ["33QJ3", "804"],
+      ["5QQQA", "105"],
+      ["3Q33A", "197"],
+      ["QQQ76", "321"],
+      ["6K888", "413"],
+      ["JQTQQ", "283"],
+      ["KT7KK", "885"],
+      ["2K666", "552"],
+      ["555KJ", "528"],
+      ["44482", "703"],
+      ["52242", "216"],
+      ["2A322", "38"],
+      ["7TQQQ", "515"],
+      ["66JK6", "812"],
+      ["442J4", "727"],
+      ["888JK", "25"],
+      ["J9993", "345"],
+      ["77297", "12"],
+      ["7Q767", "976"],
+      ["TTTJ4", "689"],
+      ["99495", "826"],
+      ["AAAJ6", "122"],
+      ["K777J", "646"],
+      ["33368", "744"],
+      ["888J5", "690"],
+      ["6676Q", "251"],
+      ["K4TKK", "332"],
+      ["9TTT6", "752"],
+      ["8988T", "472"],
+      ["Q2QJQ", "396"],
+      ["588K8", "946"],
+      ["37977", "217"],
+      ["36444", "504"],
+      ["QKQTQ", "54"],
+      ["779T7", "625"],
+      ["9KKJK", "412"],
+      ["J7888", "904"],
+      ["59939", "116"],
+      ["73757", "382"],
+      ["KKQJK", "311"],
+      ["4484T", "331"],
+      ["666J5", "450"],
+      ["449J4", "886"],
+      ["K3332", "871"],
+      ["K6664", "173"],
+      ["7K774", "789"],
+      ["45559", "103"],
+      ["23622", "366"],
     ];
+    const sorted = sortRankedHands({ rankedHands: unsortedRanked });
 
-    const comparisonIndex = 1;
-
-    const sortedHands = sortHandsByComparisonIndex({
-      hands,
-      comparisonIndex,
-    });
-
-    const expectedHands: Hand[] = [
-      ["38712", "200"],
-      ["34512", "200"],
-      ["23451", "300"],
-      ["12345", "100"],
-    ];
-
-    expect(sortedHands).toEqual(expectedHands);
+    console.log(sorted, "sorted");
   });
 });
--- day-7/index.ts
+++ day-7/index.ts
@@ -7,13 +7,29 @@ type Bid = string;
 export type Hand = [Cards, Bid];
 
 type HandType =
-  | "FiveOfAKind"
-  | "FourOfAKind"
-  | "FullHouse"
-  | "ThreeOfAKind"
-  | "TwoPair"
-  | "OnePair"
-  | "HighCard";
+  | "fiveOfAKind"
+  | "fourOfAKind"
+  | "fullHouse"
+  | "threeOfAKind"
+  | "twoPair"
+  | "onePair"
+  | "highCard";
+
+const sequence = [
+  "A",
+  "K",
+  "Q",
+  "J",
+  "T",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+];
 
 export function getHandsFromInput(path: string) {
   return fs
@@ -21,6 +37,51 @@ export function getHandsFromInput(path: string) {
     .toString()
     .split(/\r?\n/)
     .map((hand) => hand.split(" ")) as Hand[];
+}
+
+export function sortHandsByRank({ hands }: { hands: Hand[] }) {
+  const handsByRank: Record<HandType, Hand[]> = {
+    fiveOfAKind: [],
+    fourOfAKind: [],
+    fullHouse: [],
+    threeOfAKind: [],
+    twoPair: [],
+    onePair: [],
+    highCard: [],
+  };
+
+  hands.map((hand) => {
+    const handType = getHandType({ cards: hand[0] });
+    if (handType === "fiveOfAKind") {
+      handsByRank.fiveOfAKind.push(hand);
+    }
+
+    if (handType === "fourOfAKind") {
+      handsByRank.fourOfAKind.push(hand);
+    }
+
+    if (handType === "fullHouse") {
+      handsByRank.fullHouse.push(hand);
+    }
+
+    if (handType === "threeOfAKind") {
+      handsByRank.threeOfAKind.push(hand);
+    }
+
+    if (handType === "twoPair") {
+      handsByRank.twoPair.push(hand);
+    }
+
+    if (handType === "onePair") {
+      handsByRank.onePair.push(hand);
+    }
+
+    if (handType === "highCard") {
+      handsByRank.highCard.push(hand);
+    }
+  });
+
+  return handsByRank;
 }
 
 export function getHandType({ cards }: { cards: Cards }): HandType {
@@ -35,93 +96,195 @@ export function getHandType({ cards }: { cards: Cards }): HandType {
   }
 
   if (Object.values(handObj).includes(5)) {
-    return "FiveOfAKind";
+    return "fiveOfAKind";
   }
 
   if (Object.values(handObj).includes(4)) {
-    return "FourOfAKind";
+    return "fourOfAKind";
   }
 
   if (
     Object.values(handObj).includes(3) &&
     Object.values(handObj).includes(2)
   ) {
-    return "FullHouse";
+    return "fullHouse";
   }
 
   if (Object.values(handObj).includes(3)) {
-    return "ThreeOfAKind";
+    return "threeOfAKind";
   }
 
   if (Object.values(handObj).filter((el) => el === 2).length === 2) {
-    return "TwoPair";
+    return "twoPair";
   }
 
   if (Object.values(handObj).includes(2)) {
-    return "OnePair";
+    return "onePair";
   }
 
-  return "HighCard";
-}
-
-export function getComparisonIndex({ hands }: { hands: Hand[] }) {
-  let comparisonIndex: number | null = null;
-  let pointer = 0;
-
-  while (!comparisonIndex) {
-    if (pointer === 4) {
-      comparisonIndex = 4;
-    }
-
-    let cardCountObj: Record<string, number> = {};
-
-    for (const hand of hands) {
-      const cards = hand[0];
-      const card = cards[pointer];
-
-      if (!cardCountObj[card]) {
-        cardCountObj[card] = 1;
-      } else {
-        cardCountObj[card] = cardCountObj[card] + 1;
-      }
-    }
-
-    if (Object.values(cardCountObj).every((count) => count === 1)) {
-      comparisonIndex = pointer;
-      break;
-    }
-
-    pointer++;
+  if (Object.values(handObj).includes(1)) {
+    return "highCard";
   }
 
-  return comparisonIndex;
+  throw new Error("no hand type");
 }
 
-export function sortHandsByComparisonIndex({
-  hands,
-  comparisonIndex,
-}: {
-  hands: Hand[];
-  comparisonIndex: number;
-}) {
-  const sequence = [
-    "K",
-    "Q",
-    "J",
-    "10",
-    "9",
-    "8",
-    "7",
-    "6",
-    "5",
-    "4",
-    "3",
-    "2",
-    "1",
-  ];
-  return hands.sort(
-    (a, b) =>
-      sequence.indexOf(a[0][comparisonIndex]) -
-      sequence.indexOf(b[0][comparisonIndex])
+export function sortRankedHands({ rankedHands }: { rankedHands: Hand[] }) {
+  const rankedAndSorted: Hand[] = [];
+
+  const initialSort = rankedHands.sort(
+    (a, b) => sequence.indexOf(a[0][0]) - sequence.indexOf(b[0][0])
   );
+
+  const rankedByFirstCard = sequence.map((el) => {
+    return initialSort.filter((hand) => hand[0][0] === el);
+  });
+
+  rankedByFirstCard.map((rankedHandArr) => {
+    const cardType = rankedHandArr[0][0][0];
+
+    const grouped = groupByConsecutive({
+      char: cardType,
+      hands: rankedHandArr,
+    });
+
+    rankedAndSorted.push(...grouped[5]);
+    rankedAndSorted.push(...grouped[4]);
+    rankedAndSorted.push(...grouped[3]);
+    rankedAndSorted.push(...grouped[2]);
+    rankedAndSorted.push(...grouped[1]);
+  });
+
+  return rankedAndSorted;
+}
+
+export function groupByConsecutive({
+  char,
+  hands,
+}: {
+  char: string;
+  hands: Hand[];
+}) {
+  const fives: Hand[] = [];
+  const fours: Hand[] = [];
+  const threes: Hand[] = [];
+  const twos: Hand[] = [];
+  const ones: Hand[] = [];
+
+  hands.map((hand) => {
+    if (
+      hand[0][0] === char &&
+      hand[0][1] === char &&
+      hand[0][2] === char &&
+      hand[0][3] === char &&
+      hand[0][4] === char
+    ) {
+      fives.push(hand);
+    }
+
+    if (
+      hand[0][0] === char &&
+      hand[0][1] === char &&
+      hand[0][2] === char &&
+      hand[0][3] === char &&
+      hand[0][4] !== char
+    ) {
+      fours.push(hand);
+    }
+
+    if (
+      hand[0][0] === char &&
+      hand[0][1] === char &&
+      hand[0][2] === char &&
+      hand[0][3] !== char
+    ) {
+      threes.push(hand);
+    }
+
+    if (hand[0][0] === char && hand[0][1] === char && hand[0][2] !== char) {
+      twos.push(hand);
+    }
+
+    if (hand[0][0] === char && hand[0][1] !== char) {
+      ones.push(hand);
+    }
+  });
+
+  return {
+    char,
+    5: fives,
+    4: fours.sort(
+      (a, b) => sequence.indexOf(a[0][4]) - sequence.indexOf(b[0][4])
+    ),
+    3: threes.sort(
+      (a, b) => sequence.indexOf(a[0][3]) - sequence.indexOf(b[0][3])
+    ),
+    2: twos.sort(
+      (a, b) => sequence.indexOf(a[0][2]) - sequence.indexOf(b[0][2])
+    ),
+    1: ones.sort(
+      (a, b) => sequence.indexOf(a[0][1]) - sequence.indexOf(b[0][1])
+    ),
+  };
+}
+
+console.log(
+  determineTotalWinnings({
+    hands: getHandsFromInput("./input.txt"),
+  })
+);
+
+function determineTotalWinnings({ hands }: { hands: Hand[] }) {
+  const sorted: Hand[] = [];
+
+  const rankedHands = sortHandsByRank({ hands });
+
+  sorted.push(...rankedHands.fiveOfAKind);
+
+  const sortedFourOfAKind = sortRankedHands({
+    rankedHands: rankedHands.fourOfAKind,
+  });
+
+  sorted.push(...sortedFourOfAKind);
+
+  const sortedFullHouse = sortRankedHands({
+    rankedHands: rankedHands.fullHouse,
+  });
+
+  sorted.push(...sortedFullHouse);
+
+  const sortedThreeOfAKind = sortRankedHands({
+    rankedHands: rankedHands.threeOfAKind,
+  });
+
+  sorted.push(...sortedThreeOfAKind);
+
+  const sortedTwoPair = sortRankedHands({
+    rankedHands: rankedHands.twoPair,
+  });
+
+  sorted.push(...sortedTwoPair);
+
+  const sortedOnePair = sortRankedHands({
+    rankedHands: rankedHands.onePair,
+  });
+
+  sorted.push(...sortedOnePair);
+
+  const sortedHighCard = sortRankedHands({
+    rankedHands: rankedHands.highCard,
+  });
+
+  sorted.push(...sortedHighCard);
+
+  console.log(sorted.length);
+
+  const total = sorted
+    .map((hand) => hand[1])
+    .reduce(
+      (acc, bid, index) => acc + parseInt(bid) * (sorted.length - index),
+      0
+    );
+
+  return total;
 }
